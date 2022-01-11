056d072efbcc37b80e6fce044a5c54c50c4c1762
Drinnn <pedro_lontro@hotmail.com>
2022-01-11 15:01:39 -0300

    feat: solution to Porto União to Curitiba graph with Greedy Search

--- search_algorithms/greedy_search/greedy_search_test.py
+++ search_algorithms/greedy_search/greedy_search_test.py
@@ -1,6 +1,11 @@
-from arad_to_bucharest_graph import Graph
+from arad_to_bucharest_graph import Graph as BucharestGraph
+from porto_uniao_to_curitiba_graph import Graph as CuritibaGraph
 from greedy_search import GreedySearch
 
-graph = Graph()
-search = GreedySearch(graph.bucharest)
-search.search(graph.arad)
+arad_bucharest_graph = BucharestGraph()
+search = GreedySearch(arad_bucharest_graph.bucharest)
+search.search(arad_bucharest_graph.arad)
+
+porto_uniao_curitiba_graph = CuritibaGraph()
+search = GreedySearch(porto_uniao_curitiba_graph.curitiba)
+search.search(porto_uniao_curitiba_graph.portoUniao)
