c1350f3a84c876d3526e871ad5035dd2b6485ffa
Drinnn <pedro_lontro@hotmail.com>
2022-01-13 17:00:45 -0300

    feat: add fitness function

--- optimization_algorithms/fitness_function/representation.py
+++ optimization_algorithms/fitness_function/representation.py
@@ -1,3 +1,5 @@
+import mlrose
+
 people = [('Lisboa', 'LIS'), ('Madrid', 'MAD'),
           ('Paris', 'CDG'), ('Dublin', 'DUB'),
           ('Bruxelas', 'BRU'), ('Londres', 'LHR')]  # City name and Airport abbreviation
@@ -34,5 +36,26 @@ def print_flights(schedule):
     print('Total price: {}'.format(total_price))
 
 
+def fitness_function(schedule):
+    flight_id = -1
+    total_price = 0
+    for i in range(len(schedule) // 2):
+        origin_airport_abbreviation = people[i][1]
+        flight_id += 1
+        departure_flight = flights[(
+            origin_airport_abbreviation, destination)][schedule[flight_id]]
+        total_price += departure_flight[2]
+        flight_id += 1
+        return_flight = flights[(
+            destination, origin_airport_abbreviation)][schedule[flight_id]]
+        total_price += return_flight[2]
+
+    return total_price
+
+
 schedule = [1, 2, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
 print_flights(schedule)
+
+fitness = mlrose.CustomFitness(fitness_function)
+problem_representation = mlrose.DiscreteOpt(
+    length=12, fitness_fn=fitness, maximize=False, max_val=10)
